8f267416a9704aab3f61658b333e5710894d1f05
koppai <psthu.koppai@gmail.com>
2023-07-05 20:33:17 +0630

    Add event handler to the buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import './Calculator.css';
 import Buttons from './buttons';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const [display, setDisplay] = useState({ total: null, next: null, operation: null });
+
+  const handleClick = (e) => {
+    setDisplay(calculate(display, e.target.innerHTML));
+  };
+
+  const { total, next, operation } = display;
   return (
     <div className="calculator-container">
-      <div className="display">0</div>
-      <Buttons />
+      <div className="display">{next || operation || total || 0}</div>
+      <Buttons
+        handleClick={handleClick}
+      />
     </div>
   );
 }
