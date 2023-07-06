671bee07a876deb6da2b5e8813e2d803d0368778
koppai <psthu.koppai@gmail.com>
2023-07-06 16:58:50 +0630

    Fetch Api data and append to app component

---
+++ src/components/quote.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+
+function Quote() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [quote, setQuote] = useState([]);
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try {
+        setIsLoading(true);
+        const req = await fetch('https://api.api-ninjas.com/v1/quotes?category=success', {
+          headers: {
+            'X-Api-Key': 'SGjZWVibAPXyRKowk2HlGg==4v0H284fc9PGWwt3',
+          },
+        });
+        const res = await req.json();
+        setQuote(res[0].quote);
+      } catch (error) {
+        setHasError(true);
+      }
+      setIsLoading(false);
+    };
+    fetchQuote();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  if (hasError) {
+    return (
+      <div>
+        <h1>Something went wrong...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>{quote}</h1>
+    </div>
+  );
+}
+
+export default Quote;
