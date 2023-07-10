b675e47cc32f95f7c37c987985747cc5131901c8
koppai <psthu.koppai@gmail.com>
2023-07-10 21:31:00 +0630

    Create Routes for home, calculator and quote components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+/** @format */
+
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Quote from './components/quote';
+import Nav from './components/nav';
+import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
-      <Calculator />
-      <Quote />
+      <Nav />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/calculator"
+          element={<Calculator />}
+        />
+        <Route
+          path="/quote"
+          element={<Quote />}
+        />
+      </Routes>
     </div>
   );
 }
