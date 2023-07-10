646faef58824bc8417e9832026ad686278161398
koppai <psthu.koppai@gmail.com>
2023-07-10 22:20:31 +0630

    Add styling for the home component

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,22 @@
+import '../styles/home.css';
+
 function Home() {
   return (
-    <>
-      <h1>Welcome to our page</h1>
-    </>
+    <main>
+      <h1>Welcome to our page!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        voluptatum, voluptate, quibusdam, quia voluptas quod quos
+        exercitationem voluptatem quas quidem doloribus. Quisquam, quia
+        voluptas quod quos exercitationem voluptatem quas quidem doloribus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        voluptatum, voluptate, quibusdam, quia voluptas quod quos
+        exercitationem voluptatem quas quidem doloribus. Quisquam, quia
+        voluptas quod quos exercitationem voluptatem quas quidem doloribus.
+      </p>
+    </main>
   );
 }
 export default Home;
